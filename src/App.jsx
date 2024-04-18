1079bd6d478356498537e3a2cc6a372c4e4f80c2
iamosharry <140787154+iamosharry@users.noreply.github.com>
2024-04-18 05:40:30 -0700

    Update deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import smile from './assets/smiling-burger.png'
+// import smile from './assets/smiling-burger.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -15,7 +15,7 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-          <img src={smile} className="logo react" alt="React logo" />
+          <img src="src/asssets/smiling-burger.png" className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
