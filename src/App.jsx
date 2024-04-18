ea09520c1136d4ef787a5b31c923e7413ff2a814
Olajide Samuel <iamosharry@gmail.com>
2024-04-18 06:12:29 -0700

    testing others

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <h1>TEXT ARE WORKING</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
           
